feed7363773f556cebd76cba0a49d022c0267d8e
Bailey Nahi <bailey@that.global>
2025-11-26 02:50:21 +1000

    Update to macOS 13 requirement

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Web3Swift",
     platforms: [
-        .macOS(.v10_15), .iOS(.v16)
+        .macOS(.v13), .iOS(.v16)
     ],
     products: [
         .library(name: "Web3Swift", targets: ["Web3Swift"])
